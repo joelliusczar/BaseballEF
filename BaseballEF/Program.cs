499003dfda4a34d96eeb8d33c0f03a737e776b34
Joel Pridgen <joelliuscaesar@gmail.com>
2017-10-26 00:09:27 -0400

    Add project files.

---
+++ BaseballEF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
